0a86887cce22d853ab3cb5a6e36e3c2245789300
Armaddox <al241618@alumnos.uacj.mx>
2024-05-25 19:45:32 -0600

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function updateTime() {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+    const formattedDate = now.toLocaleDateString('es-ES', options);
+
+    document.getElementById("header-info").innerText = "Fecha y hora actual: " + formattedDate;
+}
+setInterval(updateTime, 1000);
+updateTime();
+
 const questions = [
     {
         question: "¿Cuál es el personaje principal en 'The Legend of Zelda'?",
